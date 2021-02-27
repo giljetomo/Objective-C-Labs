bdf365ecdd184310b8ad89b253ae2b08c0d7d2b9
Macbook Pro <gmjetomo@outlook.com>
2021-02-27 10:15:53 -0800

    feat: user story - I want to be able to roll the dice

--- Lab5 - Threelow Dice/main.m
+++ Lab5 - Threelow Dice/main.m
@@ -7,16 +7,30 @@
 
 #import <Foundation/Foundation.h>
 #import "Dice.h"
+#import "InputHandler.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Dice *d1 = [[Dice alloc] init];
         Dice *d2 = [[Dice alloc] init];
         Dice *d3 = [[Dice alloc] init];
+        Dice *d4 = [[Dice alloc] init];
+        Dice *d5 = [[Dice alloc] init];
         
-        NSLog(@"%d", d1.randomize);
-        NSLog(@"%d", d2.randomize);
-        NSLog(@"%d", d3.randomize);
+        BOOL gameOn = YES;
+        
+        while(gameOn) {
+            NSString *input = [InputHandler prompt:@"\n'roll' to roll the dice\n'hold' to hold a dice\n'reset' to un-hold all dice\n'done' to end the game\n'display' to show current stats" andSize:255];
+            if([input.lowercaseString isEqualToString: @"roll"]) {
+                NSLog(@"%d", d1.randomize);
+                NSLog(@"%d", d2.randomize);
+                NSLog(@"%d", d3.randomize);
+                NSLog(@"%d", d4.randomize);
+                NSLog(@"%d", d5.randomize);
+            }
+        }
+        
+
     }
     return 0;
 }
